6143b853444e1d7dd2bcb0c0bdc09eeafa4cebc2
greg-beach <gbeach22@gmail.com>
2025-08-11 12:19:16 -0400

    program now requires a user prompt to run

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
 def main():
     load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant\n")
+        print("Usage: python main.py \"your prompt here\"\n")
+        print("Example: python main.py \"How do I build a calculator app?\"\n")
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+   
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum add")
+   
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
+    print("Respons:\n")
     print(f"{response.text}\n")
 
 
