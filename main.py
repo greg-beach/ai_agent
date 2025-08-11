dfb76bda3d13ce40f669718e009f0764d2072468
greg-beach <gbeach22@gmail.com>
2025-08-11 12:39:36 -0400

    added option to include --verbose to get more information from the response or just the response if excluded

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant\n")
@@ -16,15 +21,23 @@ def main():
         sys.exit(1)
 
     user_prompt = " ".join(args)
+
+    verbose = False
+    if args[-1] == "--verbose":
+        verbose = True 
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
    
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
    
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
-    print("Respons:\n")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:\n")
     print(f"{response.text}\n")
 
 
