5b76ff4aa0e182247cb13fb6e3093d49eb69a484
greg-beach <gbeach22@gmail.com>
2025-08-15 14:19:39 -0400

    added method to write files in the working directory

--- tests.py
+++ tests.py
@@ -1,40 +1,17 @@
-# from functions.get_files_info import get_files_info
-from functions.get_file_content import get_files_content
+from functions.write_files import write_file
 
 def test():
-    # result = get_files_info("calculator", ".")
-    # print("Result for current directory:")
-    # print(result)
-    # print("")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print(result)
+    print("")
+   
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print(result)
+    print("")
 
-    # result = get_files_info("calculator", "pkg")
-    # print("Result for 'pkg' directory:")
-    # print(result)
-
-    # result = get_files_info("calculator", "/bin")
-    # print("Result for '/bin' directory:")
-    # print(result)
-
-    # result = get_files_info("calculator", "../")
-    # print("Result for '../' directory:")
-    # print(result)
-
-    result = get_files_content("calculator", "main.py")
-    print("Result for current directory:")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
     print("")
     
-    result = get_files_content("calculator", "pkg/calculator.py")
-    print(result)
-    print("")
-
-    result = get_files_content("calculator", "/bin/cat")
-    print(result)
-    print("")
-
-    result = get_files_content("calculator", "pkg/does_not_exist.py")
-    print(result)
-    print("")
-
 if __name__ == "__main__":
     test()
